141bce2c949abf77353b51a926415484bc8fed7d
Dung Le Anh <dungla4@fsoft.com.vn>
2018-12-13 15:27:58 +0700

    deploy

---
+++ precache-manifest.2c18e4fa7442706e88579899ec073858.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/finance-hd/robots.txt"
+  },
+  {
+    "revision": "6d0f742e67442f2019df",
+    "url": "/finance-hd/js/chunk-vendors.4057b42b.js"
+  },
+  {
+    "revision": "6471f98172f1019f956f",
+    "url": "/finance-hd/js/app.dae6b214.js"
+  },
+  {
+    "revision": "ca9fc194b356ca69a5d6",
+    "url": "/finance-hd/js/about.bcdb76fb.js"
+  },
+  {
+    "revision": "c981415f86f1b1da0338725e3e6cfb31",
+    "url": "/finance-hd/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/finance-hd/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "6d0f742e67442f2019df",
+    "url": "/finance-hd/css/chunk-vendors.677434cb.css"
+  },
+  {
+    "revision": "6471f98172f1019f956f",
+    "url": "/finance-hd/css/app.bc50ac87.css"
+  }
+];
